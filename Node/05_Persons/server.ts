a5c67baf2a3fef9e76b05f5b7f103eb2408eb5fa
unknown <micheleghis@gmail.com>
2021-10-12 08:53:36 +0200

    Update 05

--- Node/05_Persons/server.ts
+++ Node/05_Persons/server.ts
@@ -14,7 +14,6 @@ console.log("Server in ascolto sulla porta " + port)
 // Registrazione dei servizi
 // -------------------------
 dispatcher.addListener("GET", "/api/nazioni", function (req, res) {
-    res.writeHead(200, HEADERS.json)
     let nazioni = []
     for (const person of persons["results"]) {
         if(!nazioni.includes(person.location.country)) {
@@ -22,6 +21,26 @@ dispatcher.addListener("GET", "/api/nazioni", function (req, res) {
         }
     }
     nazioni.sort()
+    res.writeHead(200, HEADERS.json)
     res.write(JSON.stringify({"nazioni":nazioni}))
+    res.end()
+})
+
+dispatcher.addListener("GET", "/api/persone", function (req, res) {
+    let nazione:string =  req["GET"].nazione;
+    let vetPersons:object[] = []
+    for (const person of persons.results) {
+        if (person.location.country == nazione) {
+            let jsonPerson = {
+                "name": person.name.title + " " + person.name.first + " " + person.name.last,
+                "city": person.location.city,
+                "state": person.location.state,
+                "cell": person.cell
+            }
+            vetPersons.push(jsonPerson)
+        }
+    }
+    res.writeHead(200, HEADERS.json)
+    res.write(JSON.stringify(vetPersons))
     res.end()
 })
--- Node/05_Persons/static/index.js
+++ Node/05_Persons/static/index.js
@@ -10,6 +10,35 @@ $(document).ready(function() {
     let requestNazioni = inviaRichiesta("GET","/api/nazioni");
     requestNazioni.fail(errore)
     requestNazioni.done(function (data) {
-        console.log(data)  
+        // console.log(data)  
+        for (let i = 0; i < data.nazioni.length; i++) {
+            $('<a>', {
+                'class':'dropdown-item',
+                'href':'#',
+                'text':data.nazioni[i],
+                'click':visualizzaPersone
+            }).appendTo(_lstNazioni)
+        }
     })
+
+    function visualizzaPersone() {
+        let nation = $(this).text()
+        // console.log(nation)
+        let requestPersone = inviaRichiesta("GET","/api/persone", {"nazione":nation});
+        requestPersone.fail(errore)
+        requestPersone.done(function (persons) {
+            console.log(persons)
+            _tabStudenti.empty()
+            for (const person of persons) {
+                let tr = $("<tr>").appendTo(_tabStudenti)
+                for (const key in person) {
+                    $("<td>").appendTo(tr).html(person[key])
+                }
+                let td = $("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("dettagli")
+                td = $("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("elimina")
+            }
+        })
+    }
 })
