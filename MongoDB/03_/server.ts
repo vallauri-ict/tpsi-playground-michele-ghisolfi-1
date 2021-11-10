cdf2320182d24a622ad933c20f187c5db164c741
unknown <micheleghis@gmail.com>
2021-11-10 09:49:33 +0100

    02 - new query

--- MongoDB/03_/server.ts
+++ MongoDB/03_/server.ts
@@ -15,11 +15,13 @@ mongoClient.connect(CONNECTIONSTRING, function (err, client) {
     let db = client.db(DBNAME);
     let collection = db.collection("unicorns");
     // nome di campi sempre con $ se usati come valore (a sinistra)
-    let rq = collection.aggregate([
-      { $match: { status: "A" } },
-      { $group: { _id: "$cust_id", totale: { $sum: "$amount" } } }, // tabella fatta solo con queste due colonne
-      { $sort: { totale: -1 } }
-    ]).toArray();
+    let rq = collection
+      .aggregate([
+        { $match: { status: "A" } },
+        { $group: { _id: "$cust_id", totale: { $sum: "$amount" } } }, // tabella fatta solo con queste due colonne
+        { $sort: { totale: -1 } },
+      ])
+      .toArray();
     rq.then(function (data) {
       console.log("Query 1b - ", "N record: " + data.length, data);
     });
@@ -40,13 +42,19 @@ mongoClient.connect(CONNECTIONSTRING, function (err, client) {
     let db = client.db(DBNAME);
     let collection = db.collection("unicorns");
     // nome di campi sempre con $ se usati come valore (a sinistra)
-    let rq = collection.aggregate([
-      { $match: { status: "A" } },
-      { $group: { _id: "$cust_id", totale: { $sum: "$amount" } } }, // tabella fatta solo con queste due colonne
-      { $sort: { totale: -1 } }
-    ]).toArray();
+    let rq = collection
+      .aggregate([
+        {
+          $group: {
+            _id: "$cust_id",
+            avgAmount: { $avg: "$amount" },
+            avgTotal: {""}, // todo
+          },
+        },
+      ])
+      .toArray();
     rq.then(function (data) {
-      console.log("Query 1b - ", "N record: " + data.length, data);
+      console.log("Query 2 - ", data);
     });
     rq.catch(function (err) {
       console.log("Errore esecuzione query " + err.message);
