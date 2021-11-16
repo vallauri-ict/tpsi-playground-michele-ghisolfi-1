83cec328d4b2321c9feec66dad62c8ab131fb721
unknown <micheleghis@gmail.com>
2021-11-16 08:50:35 +0100

    New query implementation

--- MongoDB/03_/server.ts
+++ MongoDB/03_/server.ts
@@ -48,13 +48,138 @@ mongoClient.connect(CONNECTIONSTRING, function (err, client) {
           $group: {
             _id: "$cust_id",
             avgAmount: { $avg: "$amount" },
-            avgTotal: {""}, // todo
+            avgTotal: { $avg: { $multiply: ["$qta", "$amount"] } },
           },
         },
       ])
       .toArray();
     rq.then(function (data) {
       console.log("Query 2 - ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("Errore connessione al db");
+  }
+});
+
+// query 3
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    // nome di campi sempre con $ se usati come valore (a sinistra)
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        {
+          $group: {
+            _id: "$gender", // indica il campo su cui fare i gruppi
+            totale: { $sum: 1 },
+          },
+        },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 3 - ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("Errore connessione al db");
+  }
+});
+
+// query 4
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    // nome di campi sempre con $ se usati come valore (a sinistra)
+    let rq = collection
+      .aggregate([
+        {
+          $group: {
+            _id: { gender: "$gender" }, // indica il campo su cui fare i gruppi
+            mediaVampiri: { $avg: "$vampires" },
+          },
+        },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 4 - ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("Errore connessione al db");
+  }
+});
+
+// query 5
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    // nome di campi sempre con $ se usati come valore (a sinistra)
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        {
+          $group: {
+            _id: { gender: "$gender", hair: "$hair" },
+            nEsemplari: { $sum: 1 },
+          },
+        },
+        { $sort: { nEsemplari: -1, _id: -1 } },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 5 - ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("Errore connessione al db");
+  }
+});
+
+// query 6
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    // nome di campi sempre con $ se usati come valore (a sinistra)
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        {
+          $group: {
+            _id: { gender: "$gender", hair: "$hair" },
+            nEsemplari: { $sum: 1 },
+          },
+        },
+        { $sort: { nEsemplari: -1, _id: -1 } },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 6 - ", data);
     });
     rq.catch(function (err) {
       console.log("Errore esecuzione query " + err.message);
