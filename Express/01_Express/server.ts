3f79fd1c59e3b01f85a687a6feb7a99fcbb3e2b4
unknown <micheleghis@gmail.com>
2021-12-02 11:13:19 +0100

    Express template complete

--- Express/01_Express/server.ts
+++ Express/01_Express/server.ts
@@ -1,26 +1,135 @@
-import express from 'express'
-import * as _http from "http"
-import HEADERS from "./headers.json"
+import express from "express";
+import * as http from "http";
+import * as fs from "fs";
+import * as body_parser from "body-parser";
+import HEADERS from "./headers.json";
+// mongo
+import * as mongodb from "mongodb";
+const mongoClient = mongodb.MongoClient;
+const CONNECTIONSTRING =
+  "mongodb+srv://admin:admin@cluster0.tcmi5.mongodb.net/5B?retryWrites=true&w=majority";
+const DBNAME = "5B";
 
-let port:number = 1337
-let app = express()
+let port: number = 1337;
+let app = express();
 
 // app richiamata ogni volta che arriva una richiesta
-let server = _http.createServer(app)
+let server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log("Server in ascolto sulla porta " + port)
-})
+  console.log("Server in ascolto sulla porta " + port);
+  init();
+});
 
-// elenco delle routes
+let paginaErrore = "";
+function init() {
+  fs.readFile("./static/error.html", (err, data) => {
+    if (!err) {
+      paginaErrore = data.toString();
+    } else {
+      paginaErrore = "<h2> Risorsa non trovata </h2>";
+    }
+  });
+}
 
-// ogni metodo = app.use
-// ogni risorsa = *
-app.use("*", (req, res, next) => {
-    console.log(" --->" + req.method + ": " + req.originalUrl)
-    next()
-})
+// *********************************************************
+// elenco delle routes di tipo middleware
+// *********************************************************
+// 1.log
+app.use("/", (req, res, next) => {
+  console.log(" ---> " + req.method + ": " + req.originalUrl);
+  next();
+});
 
-app.get("*", (req, res, next) => {
-    res.send("this is the response")
-})
+// 2.static route
+// next() in automatico quando non trova la risorsa
+app.use("/", express.static("./static"));
+
+// 3.route lettura parametri passati nella richiesta
+app.use("/", body_parser.json());
+app.use("/", body_parser.urlencoded({ extended: true }));
+
+// 4.log parametri
+app.use("/", (req, res, next) => {
+  if (Object.keys(req.query).length > 0) {
+    console.log("      Parametri GET :>> ", req.query);
+  }
+  if (Object.keys(req.body).length > 0) {
+    console.log("      Parametri BODY :>> ", req.body);
+  }
+  next();
+});
+
+// *********************************************************
+// elenco delle routes di risposta al client
+// *********************************************************
+app.use("/", (req, res, next) => {
+  mongoClient.connect(CONNECTIONSTRING, (err, client) => {
+    if (err) {
+      res.status(503).send("Db connection error");
+    } else {
+      console.log("Connected successfully");
+      req["client"] = client;
+      next();
+    }
+  });
+});
+
+app.get("/api/risorsa1", (req, res, next) => {
+  let unicorn = req.query.nome;
+  if (unicorn) {
+    let db = req["client"].db(DBNAME) as mongodb.Db;
+    let collection = db.collection("unicorns");
+    let rq = collection.find({ name: unicorn }).toArray();
+    rq.then(function (data) {
+      res.send(data);
+    });
+    rq.catch(function (err) {
+      res.status(503).send("Query syntax error");
+    });
+    rq.finally(function () {
+      req["client"].close();
+    });
+  } else {
+    res.status(402).send("missing parameter: UnicornName");
+    req["client"].close();
+  }
+});
+
+app.patch("/api/risorsa1", (req, res, next) => {
+  let unicorn = req.body.nome;
+  let vampires = req.body.vampires;
+  if (unicorn) {
+    let db = req["client"].db(DBNAME) as mongodb.Db;
+    let collection = db.collection("unicorns");
+    let rq = collection.updateOne(
+      { name: unicorn },
+      { $inc: { vampires: vampires } }
+    );
+    rq.then(function (data) {
+      res.send(data);
+    });
+    rq.catch(function (err) {
+      res.status(503).send("Query syntax error");
+    });
+    rq.finally(function () {
+      req["client"].close();
+    });
+  } else {
+    res.status(402).send("missing parameter: UnicornName");
+    req["client"].close();
+  }
+});
+
+// *********************************************************
+// default route(risorsa non trovata) e route gestione errori
+// *********************************************************
+app.use("/", (req, res, next) => {
+  res.status(404);
+  if (req.originalUrl.startsWith("/api/")) {
+    res.send("Servizio non trovato");
+  } else {
+    // è una  pagina
+    res.send(paginaErrore);
+  }
+});
--- Express/01_Express/static/index.js
+++ Express/01_Express/static/index.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 
     $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("post", "/api/servizio1?id=5", { "nome": "pippo" });
+        let request = inviaRichiesta("get", "/api/risorsa1", { "nome": "Aurora" });
         request.fail(errore);
         request.done(function(data) {
-            alert(JSON.stringify(data));
+            if (data.length > 0) {
+                alert(JSON.stringify(data));
+            } else {
+                alert("Corrispondenza non trovata")
+            }
+            
         });
     });
 
     $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("get", "/api/servizio2", { "nome": "pluto" });
+        let request = inviaRichiesta("patch", "/api/risorsa1", { "nome": "unico", "vampires": 3 });
         request.fail(errore);
         request.done(function(data) {
-            alert(JSON.stringify(data));
+            if (data.modifiedCount > 0) {
+                alert("Aggiornamento eseguito correttamente")
+            } else  {
+                alert("Nessuna corrispindenza trovata")
+            }
         });
     });
 });
