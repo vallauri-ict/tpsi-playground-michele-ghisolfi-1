4438ab5ef339b17cd81a6026da522fdd679434dc
unknown <micheleghis@gmail.com>
2022-01-19 08:49:10 +0100

    update

--- Express/02_CrudServer/server.ts
+++ Express/02_CrudServer/server.ts
@@ -2,222 +2,265 @@ import express from "express";
 import * as http from "http";
 import * as fs from "fs";
 import * as body_parser from "body-parser";
-import HEADERS from "./headers.json";
-// mongo
 import * as mongodb from "mongodb";
-const mongoClient = mongodb.MongoClient;
-const CONNECTIONSTRING =
-  "mongodb+srv://admin:admin@cluster0.tcmi5.mongodb.net/5B?retryWrites=true&w=majority";
+
+// munga
+const mongo_client = mongodb.MongoClient;
+const CONNECTIONSTRING = "mongodb+srv://admin:admin@cluster0.jit30.mongodb.net/5B?retryWrites=true&w=majority";
 const DBNAME = "5B";
 
-let port: number = 1337;
+const PORT:number = parseInt(process.env.PORT) || 1337;
 let app = express();
 
-// app richiamata ogni volta che arriva una richiesta
 let server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log("Server in ascolto sulla porta " + port);
-  init();
+// questa collback va in esecuzione quando il server ascolta
+server.listen(PORT, () => {
+    console.log("Server in ascolto sulla porta " + PORT);
+    init();
 });
 
-let paginaErrore = "";
-function init() {
-  fs.readFile("./static/error.html", (err, data) => {
-    if (!err) {
-      paginaErrore = data.toString();
-    } else {
-      paginaErrore = "<h2> Risorsa non trovata </h2>";
-    }
-  });
-}
+let pagina_errore = "";
+let init = () => {
+    fs.readFile("./static/error.html", (err, data) => {
+        if(!err) {
+            // data è un buffer, occorre serializzarlo
+            pagina_errore = data.toString();
+        } else {
+            pagina_errore = "<h2>Risorsa non trovata</h2>";
+        };
+    });
+};
 
-// *********************************************************
-// elenco delle routes di tipo middleware
-// *********************************************************
-// 1.log
+
+
+
+
+
+/* ******************************************************************
+                elenco delle route di tipo middleware
+   ****************************************************************** */
+// 1 - log
 app.use("/", (req, res, next) => {
-  console.log(" ---> " + req.method + ": " + req.originalUrl);
-  next();
+    console.log(" -----> " + req.method + " : " + req.originalUrl);
+    next();
 });
 
-// 2.static route
-// next() in automatico quando non trova la risorsa
+// 2 - static-route
+// .static va a cercare la risorsa all'interno
+// della cartella static
 app.use("/", express.static("./static"));
 
-// 3.route lettura parametri passati nella richiesta
+// 3 - route di lettura dei parametri post
 app.use("/", body_parser.json());
-app.use("/", body_parser.urlencoded({ extended: true }));
+// extend fa in modo che vengano intercettati
+// eventuali json all'interno del body
+app.use("/", body_parser.urlencoded({ "extended": true }));
 
-// 4.log parametri
+// 4 - log dei parametri
 app.use("/", (req, res, next) => {
-  if (Object.keys(req.query).length > 0) {
-    console.log("      Parametri GET :>> ", req.query);
-  }
-  if (Object.keys(req.body).length > 0) {
-    console.log("      Parametri BODY :>> ", req.body);
-  }
-  next();
-});
-
-// *********************************************************
-// elenco delle routes di risposta al client
-// *********************************************************
-// middleware apertura connessione
-app.use("/", (req, res, next) => {
-  mongoClient.connect(CONNECTIONSTRING, (err, client) => {
-    if (err) {
-      res.status(503).send("Db connection error");
-    } else {
-      console.log("Connected successfully");
-      req["client"] = client;
-      next();
+    if(Object.keys(req.query).length) {
+        console.log("parametri GET: ", req.query);
     }
-  });
+
+    if(Object.keys(req.body).length) {
+        console.log("parametri BODY: ", req.body);
+    };
+    next();
 });
 
-// lettura collezioni presenti nel db
-app.get("/api/getCollections", (req, res, next) => {
-  let db = req["client"].db(DBNAME) as mongodb.Db;
-  let collections = db.listCollections();
-  let rq = collections.toArray();
-  rq.then((data) => {
-    res.send(data);
-  });
-  rq.catch((err) => {
-    res.status(503).send("Query syntax error");
-  });
-  rq.finally(() => {
-    req["client"].close();
-  });
+
+
+
+
+
+/* ******************************************************************
+               elenco delle route di risposta al client
+   ****************************************************************** */
+// middleware di apertura della connessione
+app.use("/", (req, res, next) => {
+    mongo_client.connect(CONNECTIONSTRING, (err, client) => {
+        if (err) {
+            res.status(503).send("Errore di connessione al database");
+        } else {
+            console.log(" >>>>>> CONNESSIONE DB ESEGUITA CORRETTAMENTE");
+            req["client"] = client;
+            next();
+        }
+    });
 });
 
-// middleware intercettazione parametri
+// lettura delle collezioni presenti nel db
+app.get("/api/getCollections", function (req, res, next) {
+    // in req["client"] salviamo il client per le connessioni successive 
+    // l' oggetto che gestisce la connessione con mongo
+    let db = req["client"].db(DBNAME) as mongodb.Db;
+
+    let request = db.listCollections().toArray();
+
+    request.then((data) => {
+        res.send(data);
+    });
+
+    request.catch((err) => {
+        res.status(503).send("Errore nella query");
+    });
+
+    request.finally(() => {
+        req["client"].close();
+    });
+});
+
+
+// middleware di intercettazione dei parametri
 let currentCollection = "";
 let id = "";
-app.use("/api/:collection", (req, res, next) => {
-  currentCollection = req.params.collection;
-  next();
-});
-app.use("/api/:collection/:id", (req, res, next) => {
-  currentCollection = req.params.collection;
-  id = req.params.id;
-  next();
+
+// il ? rende un parametro facoltativo
+app.use("/api/:collection/:id?", function (req, res, next) {
+    currentCollection = req.params.collection;
+    id = req.params.id;
+    next();
 });
 
-// listener specifici:
-// listener GET
-// listener GET. Ascolto su qualsiasi richiesta GET
-app.get("/api/*", (req, res, next) => {
-  // if sui parametri non necessaria, perchè se mancano non entra proprio nella route
-  let db = req["client"].db(DBNAME) as mongodb.Db;
-  let collection = db.collection(currentCollection);
-  if (!id) {
-    let rq = collection.find().project({ _id: 1, name: 1 }).toArray();
-
-    rq.then(function (data) {
-      res.send(data);
-    });
-    rq.catch(function (err) {
-      res.status(503).send("Query syntax error");
-    });
-    rq.finally(function () {
-      req["client"].close();
-    });
-  } else {
-    let oid = new mongodb.ObjectId(id);
-    let rq = collection.findOne({ _id: oid });
-    rq.then(function (data) {
-      res.send(data);
-    });
-    rq.catch(function (err) {
-      res.status(503).send("Query syntax error");
-    });
-    rq.finally(function () {
-      req["client"].close();
-    });
-  }
-});
-
-app.delete("/api/*", (req, res, next) => {
-  let db = req["client"].db(DBNAME) as mongodb.Db;
-  let collection = db.collection(currentCollection);
-  let _id = new mongodb.ObjectId();
-  let rq = collection.deleteOne({"_id":_id});
-
-  rq.then(function (data) {
-    res.send(data);
-  });
-  rq.catch(function (err) {
-    res.status(503).send("Query syntax error");
-  });
-  rq.finally(function () {
-    req["client"].close();
-  });
-});
-
-app.get("/api/risorsa1", (req, res, next) => {
-  let unicorn = req.query.nome;
-  if (unicorn) {
+// [[[Listeners specifici]]]:
+app.get("/api/*", function (req, res, next) {
+    // in ascolto di qualsiasi richiesta get
+    
     let db = req["client"].db(DBNAME) as mongodb.Db;
-    let collection = db.collection("unicorns");
-    let rq = collection.find({ name: unicorn }).toArray();
-    rq.then(function (data) {
-      res.send(data);
+    let collection = db.collection(currentCollection);
+    
+    let request;
+    if (!id) {
+        request = collection.find().toArray();
+    } else {
+        // va fatto solo nella else in quanto
+        // altrimenti, non esistendo id, darebbe
+        // errore
+        let obj_id = new mongodb.ObjectId(id);
+        request = collection.findOne({"_id": obj_id});
+    };
+
+    request.then((data) => {
+        res.send(data);
     });
-    rq.catch(function (err) {
-      res.status(503).send("Query syntax error");
+
+    request.catch((err) => {
+        res.status(503).send("Errore nella query");
     });
-    rq.finally(function () {
-      req["client"].close();
+
+    request.finally(() => {
+        req["client"].close();
     });
-  } else {
-    res.status(402).send("missing parameter: UnicornName");
-    req["client"].close();
-  }
 });
 
-app.patch("/api/risorsa2", (req, res, next) => {
-  let unicorn = req.body.nome;
-  let vampires = req.body.vampires;
-  if (unicorn) {
+app.post("/api/*",function(req,res,next){
+    let db = req["client"].db(DBNAME);
+    let collection = db.collection(currentCollection);
+ 
+    let request = collection.insertOne(req["body"]);
+    request.then(function(data){
+        res.send(data);
+    });
+
+    request.catch(function(err){
+        res.status(503).send("Errore esecuzione query");
+    });
+
+    request.finally(function(){
+        req["client"].close();
+    });
+});
+
+app.patch("/api/*",function(req,res,next){
+    let db = req["client"].db(DBNAME);
+    let collection = db.collection(currentCollection);
+ 
+    let obj_id = new mongodb.ObjectId(id);
+
+    let request = collection.updateOne({"_id": obj_id}, {"$set": req["body"]});
+    request.then(function(data){
+        res.send(data);
+    });
+
+    request.catch(function(err){
+        res.status(503).send("Errore esecuzione query");
+    });
+
+    request.finally(function(){
+        req["client"].close();
+    });
+});
+
+app.put("/api/*",function(req,res,next){
+    let db = req["client"].db(DBNAME);
+    let collection = db.collection(currentCollection);
+ 
+    let obj_id = new mongodb.ObjectId(id);
+
+    let request = collection.replaceOne({"_id": obj_id}, {"$set": req["body"]});
+    request.then(function(data){
+        res.send(data);
+    });
+
+    request.catch(function(err){
+        res.status(503).send("Errore esecuzione query");
+    });
+
+    request.finally(function(){
+        req["client"].close();
+    });
+});
+ 
+app.delete("/api/*", function (req, res, next) {
     let db = req["client"].db(DBNAME) as mongodb.Db;
-    let collection = db.collection("unicorns");
-    let rq = collection.updateOne(
-      { name: unicorn },
-      { $inc: { vampires: vampires } }
-    );
-    rq.then(function (data) {
-      res.send(data);
+    let collection = db.collection(currentCollection);
+    
+    let obj_id = new mongodb.ObjectId(id);
+    let request = collection.deleteOne({"_id": obj_id});
+
+    request.then((data) => {
+        res.send(data);
     });
-    rq.catch(function (err) {
-      res.status(503).send("Query syntax error");
+
+    request.catch((err) => {
+        res.status(503).send("Errore nella query");
     });
-    rq.finally(function () {
-      req["client"].close();
+
+    request.finally(() => {
+        req["client"].close();
     });
-  } else {
-    res.status(402).send("missing parameter: UnicornName");
-    req["client"].close();
-  }
 });
 
-// middleware
 
-// *********************************************************
-// default route(risorsa non trovata) e route gestione errori
-// *********************************************************
+
+
+
+
+/* ******************************************************************
+   default route (risorsa non trovata) e route di gestione degli errori
+   ****************************************************************** */
 app.use("/", (req, res, next) => {
-  res.status(404);
-  if (req.originalUrl.startsWith("/api/")) {
-    res.send("Servizio non trovato");
-  } else {
-    // è una  pagina
-    res.send(paginaErrore);
-  }
+    res.status(404);
+    if(req.originalUrl.startsWith("/api/")) {
+        res.send("Risorsa non trovata");
+    } else {
+        res.send(pagina_errore);
+    };
 });
 
-// route gestione errori
-app.use("/", (err, req, res, next) => {
-  console.log("***** ERRORE CODICE SERVER ", err.message, " *****");
+
+
+
+
+
+
+
+
+
+/* ******************************************************************
+                    route di gestione degli errori
+   ****************************************************************** */
+app.use((err, req, res, next) => {
+    console.log("Errore codice server: " + err.message);
 });
--- Express/02_CrudServer/static/index.js
+++ Express/02_CrudServer/static/index.js
@@ -1,15 +1,18 @@
-"use strict";
+"use strict"
 
 $(document).ready(function () {
-  let divIntestazione = $("#divIntestazione");
-  let divCollections = $("#divCollections");
-  let table = $("#mainTable");
-  let divDettagli = $("#divDettagli");
   let filters = $(".card").first();
+  let divIntestazione = $("#divIntestazione")
+  let divCollections = $("#divCollections")
+  let table = $("#mainTable")
+  let divDettagli = $("#divDettagli")
+  
   let currentCollection = "";
 
+  filters.hide();
+
   let request = inviaRichiesta("get", "/api/getCollections");
-  request.fail(errore);
+  request.fail(errore)
   request.done(function (collections) {
     console.log(collections);
     let label = divCollections.children("label");
@@ -23,86 +26,137 @@ $(document).ready(function () {
     label.remove();
   });
 
-  divCollections.on("click", "input[type=radio]", function () {
+
+
+
+  divCollections.on("click", "input[type='radio']", function() {
     currentCollection = $(this).val();
+
+
+
     let request = inviaRichiesta("GET", "/api/" + currentCollection);
     request.fail(errore);
-    request.done(function (data) {
+    request.done((data) => {
       console.log(data);
       divIntestazione.find("strong").eq(0).text(currentCollection);
       divIntestazione.find("strong").eq(1).text(data.length);
-      if (currentCollection == "unicorns") {
+      if (currentCollection == "Unicorns") {
         filters.show();
       } else {
         filters.hide();
-      }
+      };
+
+
+      let tbody = table.children("tbody");
+      tbody.empty();
       for (const item of data) {
-        let tr = $("<tr>").appendTo(table.children("tbody"));
-        let td = $("<td>").appendTo(tr);
-        td.text(item._id).appendTo(tr);
+        let tr = $("<tr>");
+        tbody.append(tr);
 
-        td = $("<td>").appendTo(tr);
+        let td = $("<td>");
+        td.appendTo(tr);
+        td.text(item._id);
+        td.prop("_id", item._id);
+        td.prop("method", "get");
+        td.on("click", visualizzaDettagli);
+
+        td = $("<td>");
+        td.appendTo(tr);
         td.text(item.name);
+        td.prop("_id", item._id);
+        td.prop("method", "get");
+        td.on("click", visualizzaDettagli);
 
-        td = $("<td>").appendTo(tr);
-        $("<div>")
-          .appendTo(td)
-          .prop({ id: item._id, method: "patch" })
-          .on("click", elimina);
-        $("<div>").appendTo(td);
-        $("<div>").appendTo(td).prop("id", item._id).on("click", elimina);
+        td = $("<td>");
+        td.appendTo(tr);
+
+        $("<div>").appendTo(td).prop({"_id": item._id, "method": "patch"}).on("click", visualizzaDettagli);
+
+        $("<div>").appendTo(td).prop({"_id": item._id, "method": "put"}).on("click", visualizzaDettagli);
+        
+        $("<div>").appendTo(td).prop("_id", item._id).on("click", elimina);
       }
     });
   });
 
+
+
   function elimina() {
-    let request = inviaRichiesta(
-      "delete",
-      "/api/" + currentCollection + "/" + $(this).prop("_id")
-    );
+    let request = inviaRichiesta("delete", "/api/" + currentCollection + "/" + $(this).prop("_id"));
     request.fail(errore);
-    request.done(function () {
-      alert("documento rimosso correttamente");
+    request.done((data) => {
+      alert("Documento cancellato correttamente");
       aggiorna();
     });
   }
 
-  function visualizzaDettagli() {
-    //
+
+  let aggiorna = () => {
+    let event = jQuery.Event('click');
+    event.target = divCollections.find('input[type=radio]:checked')[0];
+    divCollections.trigger(event);
   }
 
-  $("#btnAdd").on("click", () => {
-    divDettagli.empty();
+  function visualizzaDettagli() {
+    // attenzione, in questo caso non sarebbe andato
+    // fare this con la arrow func, in quanto non avrebbe
+    // modificato il this
+    let method = $(this).prop("method").toUpperCase();
+    let id = $(this).prop("_id");
 
-    let txtArea = $("<textArea>");
-    txtArea.val("{ }");
-    txtArea.appendTo(divDettagli);
+    let request = inviaRichiesta("get", "/api/" + currentCollection + "/" + id);
+    request.fail(errore);
+    request.done((data) => {
+      divDettagli.empty();
+    
+      console.log(data);
+      let content = "";
+      for (const key in data) {
+        content += "<strong>" + key + ":</strong> " + data[key] + "<br>";
+      }
+      divDettagli.append(content);
+      if (method != "GET") {
+        divDettagli.empty();
+        let txtArea = $("<textarea>");
+        delete data._id;
+        txtArea.val(JSON.stringify(data, null, 2));
+        txtArea.appendTo(divDettagli);
 
+        visualizzaPulsanteInvia(method, id);
+      }
+    });
+  };
+
+  let visualizzaPulsanteInvia = (method, id = "") => {
     let btnInvia = $("<button>");
-    btnInvia.text("INVIA");
+    btnInvia.text("Invia");
     btnInvia.appendTo(divDettagli);
     btnInvia.on("click", () => {
+      alert(txtArea.val());
       let param = "";
       try {
-        let param = JSON.parse(txtArea.val());
-      } catch (error) {
-        alert("ERROR: JSON non valido");
+        param = JSON.parse(txtArea.val());
+      } catch {
+        alert("Json non valido");
         return;
       }
 
-      let req = inviaRichiesta("post", "/api/" + currentCollection, param);
-      req.fail(errore);
-      req.done(function () {
-        alert("Inserimento eseguito correttamente");
+      let request = inviaRichiesta(method, "/api/" + currentCollection + "/" + id, param);
+      request.fail(errore);
+      request.done(() => {
+        alert("operazione avvenuto correttamente");
         divDettagli.empty();
         aggiorna();
       });
     });
-  });
+  };
 
-  function aggiorna() {
-    let event = jQuery.Event("click");
-    event.target = divCollections.find("input[type=radio]:checked")[0];
-    divCollections.trigger(event);
-  }
+  $("#btnAdd").on("click", () => {
+    divDettagli.empty();
+    let txtArea = $("<textarea>");
+    txtArea.val("{ }");
+    txtArea.appendTo(divDettagli);
+
+    visualizzaPulsanteInvia("post");
+  });
 });
